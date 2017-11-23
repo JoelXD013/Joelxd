d52c28f6378f514efa74ab7b074a2772c10955e6
JoelXD013 <33781400+JoelXD013@users.noreply.github.com>
2017-11-22 16:14:32 -0900

    For Loops

---
+++ Java Coding Guide/starting out/src/For_Loops.java
@@ -0,0 +1,10 @@
+
+public class For_Loops {
+
+	public static void main(String[] args) {
+		for(int count = 3; count <= 12; count+=3) {//has three arguments inside it, first is where loop starts, second is where it ends goes until condition untrue, and third is how much to increment it
+			System.out.println(count);
+		}
+	}
+
+}
